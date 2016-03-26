e31463ef79b3ae68860edbe80dcb0cba11ae9986
Victoria Vladimirova <wiktorija.wladimirowa@gmail.com>
2016-03-26 16:15:51 +0500

    Изменены схемы, скрипт наполнения и тесты бд

--- scripts/fillDB.js
+++ scripts/fillDB.js
@@ -1,89 +1,85 @@
 'use strict';
+
 var mongoose = require('./mongooseConnect.js');
+var clearDB = require('./clearDB.js');
+var clearES = require('./clearES.js');
+
+var faker = require('faker');
+var async = require('async');
+
 var User = require('../models/users.js');
 var Quest = require('../models/quests.js').Quest;
 var Photo = require('../models/quests.js').Photo;
 var Location = require('../models/quests.js').Location;
 var Comment = require('../models/quests.js').Comment;
-var CheckIn = require('../models/check-in.js');
-var clearDb = require('./clearDB.js');
-var clearES = require('./clearES.js');
-var faker = require('faker');
-var async = require('async');
 
-const PEOPLE_COUNT = 50;
+const peopleCount = 50;
+const commentsMaxCount = 5;
 
-function fillUsers(userName, questName, callback) {
+function fillUsers(id, callback) {
     User.register(new User({
-            username: userName,
-            email: faker.internet.email(),
-            questname: questName
-        }),
-        faker.internet.password(),
-        (err, data) => {
-            if (err) {
-                callback(err);
-            } else {
-                callback();
-            }
-        });
+        _id: id,
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        phone: faker.phone.phoneNumberFormat()
+    }), faker.internet.password(), err => {
+        err ? callback(err) : callback();
+    });
 }
 
+function fillQuests(usersId, callback) {
+    var size = Math.floor(1 + Math.random() * commentsMaxCount);
+    var users = [];
+    for (var i = 0; i < size; i++) {
+        users.push(usersId[Math.floor(Math.random() * peopleCount)]);
+    }
 
-function fillQuests(userName, questName, callback) {
     new Quest({
-        name: questName,
-        author: userName,
+        name: faker.company.companyName(),
+        author: usersId[Math.floor(Math.random() * peopleCount)],
         city: faker.address.city(),
         description: faker.hacker.phrase(),
+        comments: [new Comment({
+            author: usersId[Math.floor(Math.random() * peopleCount)],
+            date: Date.now,
+            text: faker.hacker.phrase()
+        })],
         photo: new Photo({
-            quest: questName,
-            filename: faker.image.avatar(),
+            checkIn: users,
+            link: faker.image.imageUrl(),
+            description: faker.hacker.phrase(),
+            hint: faker.hacker.phrase(),
             location: new Location({
                 lat: faker.address.latitude(),
                 lon: faker.address.longitude()
             }),
             comments: [new Comment({
-                author: userName,
+                author: usersId[Math.floor(Math.random() * peopleCount)],
                 date: Date.now,
                 text: faker.hacker.phrase()
             })]
         })
-    }).save((err, data) => {
-        console.log(data);
-        if (err) {
-            callback(err);
-        } else {
-            callback();
-        }
-    });
-}
-
-function fillCheckIn(userName, questName, callback) {
-    new CheckIn({
-        username: userName,
-        photoname: faker.image.avatar(),
-        questname: questName
-    }).save((err, data) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null, true);
-        }
+    }).save(err => {
+        err ? callback(err) : callback();
     });
 }
 
 module.exports.fillDataBase = function (callback) {
-    var functionsArray = [];
-    for (var i = 0; i < PEOPLE_COUNT; i++) {
-        var questName = faker.company.companyName();
-        var userName = faker.internet.userName();
-        functionsArray = functionsArray.concat(
-            fillUsers.bind(null, userName, questName),
-            fillQuests.bind(null, userName, questName),
-            fillCheckIn.bind(null, userName, questName));
+
+    var usersId = [];
+    for (var i = 0; i < peopleCount; i++) {
+        usersId.push(mongoose.Types.ObjectId());
     }
-    async.series(functionsArray, (err) => {
+
+    var functionsArray = [];
+    for (var j = 0; j < peopleCount; j++) {
+        functionsArray = functionsArray.concat(
+            fillUsers.bind(null, usersId[j]),
+            fillQuests.bind(null, usersId)
+        );
+    }
+
+    async.parallel(functionsArray, err => {
         mongoose.disconnect(function () {
             console.log('All connections closed.');
         });
@@ -92,7 +88,7 @@ module.exports.fillDataBase = function (callback) {
 };
 
 module.exports.reFillDataBase = function (callback) {
-    clearDb(() => {
+    clearDB(() => {
         clearES(() => {
             module.exports.fillDataBase(callback);
         });
@@ -100,9 +96,5 @@ module.exports.reFillDataBase = function (callback) {
 };
 
 module.exports.reFillDataBase((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Database filled');
-    }
+    err ? console.log(err) : console.log('Database filled');
 });
