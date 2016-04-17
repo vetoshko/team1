42bce43212089846c3eaf65e3e3487b72cd8d028
Lucifer-is-my-pet <dianka-piratka@list.ru>
2016-04-17 15:21:27 +0500

    Третья попытка

--- blocks/signin-form/signin-form.js
+++ blocks/signin-form/signin-form.js
@@ -1,7 +1,7 @@
 var button = document.querySelector('.singin-search__hide');
-button.addEventListener('click', hide, false);
+button.addEventListener('click', hideAndShow, false);
 
-function hide() {
+function hideAndShow() {
     var signin = document.querySelector('.signin');
     signin.classList.toggle('signin_hidden');
 }
