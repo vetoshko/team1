67262ca23fa821538593d2a9d40c21203a5f3b9c
Victoria Vladimirova <wiktorija.wladimirowa@gmail.com>
2016-05-15 17:46:49 +0500

    страница списка квестов

--- routes/quests.js
+++ routes/quests.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('main/main');
+router.get('/', function (req, res) {
+    res.render('quests/quests', { });
 });
 
 module.exports = router;
