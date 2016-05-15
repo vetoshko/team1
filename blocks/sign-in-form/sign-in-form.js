62dd404b2565edb6b51f82c07d1033aaa45b8240
Nikita <nikshelekb@yandex.ru>
2016-05-15 11:11:13 +0500

    surge + fix '-' blocks

--- blocks/sign-in-form/sign-in-form.js
+++
@@ -1,9 +0,0 @@
-define('signin-form', [], function () {
-    return function () {
-        var signInForm = document.querySelector('.sign-in-form');
-        signInForm.classList.toggle('sign-in-form_hidden');
-
-        var signInQuestion = document.querySelector('.sign-in__question');
-        signInQuestion.classList.toggle('sign-in_hidden');
-    };
-});
