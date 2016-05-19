d283004566497c71d7fda33c3904002a90bf546c
Nikita <nikshelekb@yandex.ru>
2016-05-19 14:13:50 +0500

    quests routes update

--- routes/quests.js
+++ routes/quests.js
@@ -8,9 +8,9 @@ router.get('/', questControllers.getListPage);
 router.get('/questsList', questControllers.list);
 router.get('/:questId', questControllers.get);
 router.get('/:questId/info', questControllers.getInfo);
-router.post('/:questId', loginRequired, questControllers.edit);
-router.delete('/:questId', loginRequired, questControllers.delete);
-router.put('/:questId', loginRequired, questControllers.addPhoto);
-router.delete('/:questId/:photoId', loginRequired, questControllers.deletePhoto);
+router.post('/:questId', loginRequired(), questControllers.edit);
+router.delete('/:questId', loginRequired(), questControllers.delete);
+router.put('/:questId', loginRequired(), questControllers.addPhoto);
+router.delete('/:questId/:photoId', loginRequired(), questControllers.deletePhoto);
 
 module.exports = router;
